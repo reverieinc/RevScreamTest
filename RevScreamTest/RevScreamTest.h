b41642a57535703a2c5549edb9a6d0ede17654d5
chandanbhagabati <chandan.bhagabati@reverieinc.com>
2023-12-12 13:48:08 +0530

    Initial Commit

---
+++ RevScreamTest/RevScreamTest.h
@@ -0,0 +1,18 @@
+//
+//  RevScreamTest.h
+//  RevScreamTest
+//
+//  Created by Chandan Bhagabati on 12/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RevScreamTest.
+FOUNDATION_EXPORT double RevScreamTestVersionNumber;
+
+//! Project version string for RevScreamTest.
+FOUNDATION_EXPORT const unsigned char RevScreamTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RevScreamTest/PublicHeader.h>
+
+
